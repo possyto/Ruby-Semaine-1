9f5cda891661f11c88d429750181b9230e3e44ff
Alexandre Vanavermaete <alexandrevanavermaete@hotmail.fr>
2020-04-07 18:03:26 +0200

    17

---
+++ exo_17.rb
@@ -0,0 +1,22 @@
+puts " ton age stp  "
+
+    age = gets.chomp.to_i
+
+    i = 0
+
+while age > 0
+
+age = age - 1
+
+i = i + 1
+
+
+    
+
+puts " il y a #{age} ans  tu avais #{i} ans "
+
+ 
+
+ end 
+
+ 
